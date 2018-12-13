1f93d4f05c9200b15d2b1d1703830e9a5415f300
nils <nilscambreleng@gmail.com>
2018-12-13 16:49:02 +0100

    errors pages

--- vue-front/src/router.js
+++ vue-front/src/router.js
@@ -7,6 +7,33 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '*',
+      redirect: '/404'
+    },
+    {
+      path: '/404',
+      name: 'NotFound',
+      component: () => import('./views/errors/NotFound.vue')
+    },
+    // {
+    //   path: '/403',
+    //   meta: {
+    //     public: true,
+    //   },
+    //   name: 'AccessDenied',
+    //   component: () => import()
+    // },
+    // {
+    //   path: '/500',
+    //   name: 'ServerError',
+    //   component: () => import('')
+    // },
+    // {
+    //   path: '/login',
+    //   name: 'Login',
+    //   component: () => import('')
+    // },
+    {
       path: '/',
       name: 'home',
       component: Home
