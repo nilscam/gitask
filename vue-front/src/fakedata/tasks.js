ae1b4c46548f0c2b0386a58c5792a1a74ca7d7d7
Olivier <olivier.garcia-telouk@epitech.eu>
2018-12-13 17:51:17 +0100

    Changed color to blue

--- vue-front/src/fakedata/tasks.js
+++ vue-front/src/fakedata/tasks.js
@@ -5,20 +5,100 @@ export default [
     status: 'waiting',
     visible: true,
     id: utils.idGen(),
-    title: 'Fix front vueJs',
+    title: 'Navbar front VueJs',
     langage: 'Vuejs',
     amount: 10.00,
     commit: '5ff0dd0fe59',
-    desc: 'Increment the current Version of erer version to 2.2 for compatible rerz dsfsdfs'
+    desc: 'Make a navbar with the newest 2.5 version of VueJs'
   },
   {
     status: 'progress',
     visible: true,
     id: utils.idGen(),
-    title: 'Fix front vueJs',
-    langage: 'Vuejs',
+    title: 'Bash script for testing',
+    langage: 'bash',
+    amount: 20.00,
+    commit: 'cd12ee387ef',
+    desc: 'Create and setup unit test in bash for a C++ program'
+  },
+  {
+    status: 'waiting',
+    visible: true,
+    id: utils.idGen(),
+    title: 'Translation in AngularJS',
+    langage: 'AngularJS',
+    amount: 35.00,
+    commit: 'b4dde512eea',
+    desc: 'Setup the translation for english and spanish in a AngularJS application'
+  },
+  {
+    status: 'progress',
+    visible: true,
+    id: utils.idGen(),
+    title: 'Daily update in Electron app',
+    langage: 'NodeJS',
+    amount: 7.50,
+    commit: '4ee2a3fdafe',
+    desc: 'Create a NodeJS module that update the npm modules versions everyday'
+  },
+  {
+    status: 'done',
+    visible: true,
+    id: utils.idGen(),
+    title: 'Website Scrapping in python',
+    langage: 'Python',
+    amount: 20.00,
+    commit: '54eddfa3281',
+    desc: 'Scrap all the email of the veterinary of England'
+  },
+  {
+    status: 'progress',
+    visible: true,
+    id: utils.idGen(),
+    title: 'Encapsulation of C++',
+    langage: 'C++',
     amount: 10.00,
-    commit: '5ff0dd0fe59',
-    desc: 'Increment the current Version of erer version to 2.2 for compatible rerz dsfsdfs'
+    commit: '5eddca1221',
+    desc: 'Encapsulate the C++ code with abstract class, of my program'
+  },
+  {
+    status: 'progress',
+    visible: true,
+    id: utils.idGen(),
+    title: 'Compliancy Chrome/Mozilla css',
+    langage: 'CSS',
+    amount: 8.00,
+    commit: '1224ed18ed',
+    desc: 'Make my website compliant for Chrome and Mozilla'
+  },
+  {
+    status: 'progress',
+    visible: true,
+    id: utils.idGen(),
+    title: 'R Statistics',
+    langage: 'R',
+    amount: 35.00,
+    commit: 'ffcde12dea',
+    desc: 'Create many plot from a csv file containing values set'
+  },
+  {
+    status: 'waiting',
+    visible: true,
+    id: utils.idGen(),
+    title: 'Refactor C Legacy code',
+    langage: 'C',
+    amount: 14.00,
+    commit: 'aa4f3ee218e',
+    desc: 'Refactor an old C code of our project'
+  },
+  {
+    status: 'progress',
+    visible: true,
+    id: utils.idGen(),
+    title: 'NodeJS to Ruby',
+    langage: 'NodeJS/Ruby',
+    amount: 45.00,
+    commit: '11e2d34aefa',
+    desc: 'Translate my NodeJS project code to Ruby code'
   }
 ]
